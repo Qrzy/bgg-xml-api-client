7afc0b40e228d97f27bd8f537cad1dd8a2c96658
Qrzy <qrzy88@gmail.com>
2023-06-11 22:23:04 +0200

    feat: use unbuild for build

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 /* istanbul ignore file */
-
-import bggXmlApiClient from './client'
-
-export default bggXmlApiClient
-
 export { bggXmlApiClient } from './client'
 export * from './types'
 export * from './wrappers'
