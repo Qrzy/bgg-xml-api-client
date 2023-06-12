0c902581b3b883eaa78c1d0c99880fe2112bb2fd
Qrzy <qrzy88@gmail.com>
2023-06-12 17:12:47 +0200

    feat: use ofetch

--- src/client/getBggApiClient.ts
+++ src/client/getBggApiClient.ts
@@ -1,22 +1,37 @@
-import 'setimmediate'
-import type { AxiosInstance } from 'axios'
-import axios from 'axios'
+import type { FetchContext, FetchOptions, FetchResponse } from 'ofetch'
+import { ofetch } from 'ofetch'
 import { getBaseUrlForResource } from '../helpers/getBaseUrlForResource'
 import { xmlParser } from '../helpers/xmlParser'
 import type { ResourceName } from '../types'
 
-export function getBggApiClient(resource: ResourceName): AxiosInstance {
-  const client = axios.create({
+interface TimeoutOptions {
+  controller: AbortController
+  timeout: number // in milliseconds
+}
+
+const DEFAULT_TIMEOUT = 10000 // milliseconds
+
+export function getBggApiClient(resource: ResourceName, timeoutOptions?: TimeoutOptions) {
+  const { controller, timeout } = timeoutOptions || { controller: new AbortController(), timeout: DEFAULT_TIMEOUT }
+  const apiFetch = ofetch.create({
     baseURL: getBaseUrlForResource(resource),
     headers: {
-      'Accept': 'text/xml',
       'Content-Type': 'text/xml',
     },
-    timeout: 10000,
+    responseType: 'blob',
+    signal: controller.signal,
+    async onResponse(context: FetchContext & { response: FetchResponse<'blob'> }) {
+      const responseText = await context.response.text()
+      context.response._data = await xmlParser.parse(responseText)
+    },
   })
-  client.interceptors.response.use(async (response) => {
-    response.data = await xmlParser.parse(response.data)
-    return response
-  })
-  return client
+
+  return {
+    async get<T>(options?: FetchOptions<'blob'>) {
+      const timeoutId = setTimeout(() => controller.abort(), timeout)
+      const response = await apiFetch<T, 'blob'>(resource, options)
+      clearTimeout(timeoutId)
+      return response
+    },
+  }
 }
