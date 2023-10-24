715d83d69ac5e46c332a20de301badb986eb22dc
Qrzy <qrzy88@gmail.com>
2023-10-24 15:15:24 +0200

    feat: add a playground, in Nuxt

---
+++ playground/app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { getBggSearch } from '../src'
+
+const { data: serverData } = useFetch('/api/bggSearch/Blood-rage')
+
+const searchData = ref<any>(null)
+
+if (process.client)
+  searchData.value = await getBggSearch({ query: 'Blood rage', exact: 1, type: 'boardgame' })
+</script>
+
+<template>
+  <h1>Playground</h1>
+  <h2>Server API proxy Search data:</h2>
+  <pre>{{ serverData }}</pre>
+  <h2>Client Search data:</h2>
+  <pre>{{ searchData }}</pre>
+</template>
