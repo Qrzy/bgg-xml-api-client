2c20f792325130cf91f5281b60336f64d7726ae9
Qrzy <qrzy88@gmail.com>
2026-01-06 15:18:22 +0100

    chore: update playground code

--- playground/server/api/bggSearch/[q].get.ts
+++ playground/server/api/bggSearch/[q].get.ts
@@ -1,11 +1,12 @@
 import { getBggSearch } from '../../../../src'
+import type { H3Event } from 'h3'
 
 export default defineEventHandler(async (event: H3Event) => {
   try {
-    const result = await getBggSearch({ query: getRouterParam(event, 'q') })
+    const result = await getBggSearch({ query: getRouterParam(event, 'q') as string }, { authorizationKey: useRuntimeConfig().bggApiToken })
     return result
   }
-  catch (err) {
+  catch (err: any) {
     console.error(err)
     return {
       statusCode: 500,
